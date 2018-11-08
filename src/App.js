bb61e21670d18ca7b1d5f9a8fed3fcb03536f788
sundancekid1108 <sundancekid1108@gmail.com>
2018-11-08 19:41:06 +0900

    submodule에서 분리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+{/**
+   희한하게 git 작업하다가 
+   작업한거 다 날림....
+   
+   */
 
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Movie.css';
+
+class Movie extends Component {
+    static propTypes = {
+        title: PropTypes.string,
+        poster: PropTypes.string
+    } /*proptype. isrequired로 부모속성 파악 */
+
+    render() {
+        
+        return (
+            <div>
+            <MoviePoster poster = {this.props.poster}/>
+            <h1> {this.props.title} </h1>
+            </div>
+        )
+ 
+    }
+}
+
+class MoviePoster extends Component {
+    render() {
+        console.log(this.props);
+        return ( <img src = {this.props.poster}/> )
+    }
+}
+
+export default Movie;
